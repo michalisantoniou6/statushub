7ad812bee3b1497f7159763a863946c91ee3cfa6
michalisantoniou6 <michalisantoniou6@gmail.com>
2015-04-21 23:37:34 -0400

    Adds accent color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,7 +39,7 @@
 					@if (Auth::guest())
 
 					@else
-						<li class="dropdown">
+						<li class="dropdown top-menu">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
 								<li><a href="{{ \URL::action('FriendsController@create', [ $authUserId ]) }}">Make Friends</a></li>
