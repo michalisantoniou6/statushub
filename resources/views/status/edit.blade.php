2293b3a1a737314f53e2188ee4d80d7f6ec3fa3c
michalisantoniou6 <michalisantoniou6@gmail.com>
2015-04-21 23:17:05 -0400

    Fixes hardcoded urls in edit view

--- resources/views/status/edit.blade.php
+++ resources/views/status/edit.blade.php
@@ -4,13 +4,13 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                {!! Form::open(['route' => ['user.status.update', 7, 4], 'method' => 'PUT', 'class' => 'form']) !!}
+                {!! Form::open(['route' => ['user.status.update', $authUserId, $status->id], 'method' => 'PUT', 'class' => 'form']) !!}
 
                 <div class="form-group">
                     <h1>Change what you said...</h1>
                     {!! Form::textarea(
                     'status',
-                    isset($status) ? $status : '',
+                    isset($status->status) ? $status->status : '',
                     [
                     'required',
                     'class'=>'form-control',
